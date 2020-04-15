172056513666093fcf3879427380ff3fc8f960e2
Megan Zlock <megan.zlock@viget.com>
2020-04-15 14:53:45 -0400

    refactor headings example component plugin to use tailwind plugin func

--- examples/resp-headings/index.js
+++ examples/resp-headings/index.js
@@ -1,34 +1,37 @@
-module.exports = () => ({ addComponents, theme }) => {
+const plugin = require('tailwindcss/plugin')
+
+module.exports = plugin(({ addComponents, e, theme }) => {
   const screens = theme('screens')
+  const pluginConfig = theme('headings', {})
 
-  addComponents(
-    Object.entries(theme('headings')).map(([name, props]) => {
-      const { sizes, letterSpacing = '0', lineHeight, weight } = props
-      const mediaQueries = []
+  const component = Object.entries(pluginConfig).map(([name, props]) => {
+    const { sizes, letterSpacing = '0', lineHeight, weight } = props
+    const mediaQueries = []
 
-      for (const key in screens) {
-        if (key in sizes) {
-          mediaQueries.push({
-            [`@media (min-width: ${screens[key]})`]: {
-              [`.heading-${name}`]: {
-                'font-size': sizes[key]
-              }
+    for (const key in screens) {
+      if (key in sizes) {
+        mediaQueries.push({
+          [`@media (min-width: ${screens[key]})`]: {
+            [`.heading-${name}`]: {
+              'font-size': sizes[key]
             }
-          })
-        }
-      }
-
-      return [
-        {
-          [`.heading-${name}`]: {
-            'font-size': sizes['default'],
-            'font-weight': weight,
-            'letter-spacing': letterSpacing,
-            'line-height': lineHeight
           }
-        },
-        ...mediaQueries
-      ]
-    })
-  )
-}
+        })
+      }
+    }
+
+    return [
+      {
+        [`.heading-${name}`]: {
+          'font-size': sizes['default'],
+          'font-weight': weight,
+          'letter-spacing': letterSpacing,
+          'line-height': lineHeight
+        }
+      },
+      ...mediaQueries
+    ]
+  })
+
+  addComponents(component)
+})
