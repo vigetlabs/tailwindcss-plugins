e606b8112f67c6eb6a93dff535fc9d3c4abba4cf
Greg Kohn <greg.kohn@viget.com>
2020-04-22 14:32:56 -0400

    Add prettier and format project

--- examples/rect/index.js
+++ examples/rect/index.js
@@ -2,21 +2,21 @@ module.exports = () => ({ addUtilities, e, theme, variants }) => {
   const pluginConfig = theme('rect', {})
   const pluginVariants = variants('rect', [])
 
-  const utilities = Object.entries(pluginConfig).map(obj => {
+  const utilities = Object.entries(pluginConfig).map((obj) => {
     // obj = ['target', ['44px', '44px']]
     let valueIsArray = Array.isArray(obj[1])
 
     if (valueIsArray) {
       return {
         [`.${e(`rect-${obj[0]}`)}`]: {
-          'width': obj[1][0],
-          'height': obj[1][1],
-        }
+          width: obj[1][0],
+          height: obj[1][1],
+        },
       }
     }
   })
 
   addUtilities(utilities, {
-    variants: pluginVariants
+    variants: pluginVariants,
   })
 }
