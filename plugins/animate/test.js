6d0d011a2b3b960ed60c126ba2a96ef2601399f8
Jeremy Frank <jeremyjfrank@gmail.com>
2022-06-08 15:07:23 -0400

    Fix animate plugin test

--- plugins/animate/test.js
+++ plugins/animate/test.js
@@ -1,22 +1,31 @@
 const cssMatcher = require('jest-matcher-css')
 const plugin = require('./index')
-const { generateUtilities } = require('../../testing/generators')
+const { run } = require('../../testing/run')
 
 expect.extend({
   toMatchCss: cssMatcher,
 })
 
-test('it generates the animate classes', () => {
+it('should generate the animate classes', () => {
   const config = {
+    content: [
+      {
+        raw: String.raw`
+          <ul class="stagger-fade-left stagger-interval-200 stagger-delay-200">
+            <li class="duration-500">1</li>
+            <li class="duration-500">2</li>
+            <li class="duration-500">3</li>
+          </ul>
+        `,
+      },
+    ],
     theme: {
       animate: {
         triggerClass: '-observed',
         staggerDelay: {
-          '100': '100ms',
           '200': '200ms',
         },
         staggerInterval: {
-          default: '100ms',
           '200': '200ms',
         },
         maxItemIntervalSupport: 5,
@@ -31,74 +40,59 @@ test('it generates the animate classes', () => {
               opacity: 1,
             },
           },
-          'zoom-in': {
-            from: {
-              transform: 'scale(0.8)',
-              opacity: 0,
-            },
-          },
         },
       },
     },
   }
 
-  const output = `
+  const output = String.raw`
+    .duration-500 {
+      transition-duration: 500ms;
+    }
+
     .animate-fade-left:not(.-observed), .stagger-fade-left:not(.-observed) > * {
       transform: translateX(-20px);
       opacity: 0;
     }
 
-    .animate-fade-left.-observed, .stagger-fade-left.-observed > * {
+    .animate-fade-left.-observed,.stagger-fade-left.-observed > * {
       transform: translateX(0);
       opacity: 1;
     }
-
-    .animate-zoom-in:not(.-observed), .stagger-zoom-in:not(.-observed) > * {
-      transform: scale(0.8);
-      opacity: 0;
-    }
-
-    [class*="stagger-"] > * {
-      --stagger-delay: 0s;
-      transition-delay: calc(var(--animate-index) * 100ms + var(--stagger-delay));
-    }
-
+    
     .stagger-interval-200 > * {
       --stagger-delay: 0s;
       transition-delay: calc(var(--animate-index) * 200ms + var(--stagger-delay));
     }
-
-    .stagger-delay-100 > * {
-      --stagger-delay: 100ms;
-    }
-
+    
     .stagger-delay-200 > * {
       --stagger-delay: 200ms;
     }
-
+    
     [class*="stagger"] > *:nth-child(1) {
       --animate-index: 1;
     }
-
+    
     [class*="stagger"] > *:nth-child(2) {
       --animate-index: 2;
     }
-
+    
     [class*="stagger"] > *:nth-child(3) {
       --animate-index: 3;
     }
-
+    
     [class*="stagger"] > *:nth-child(4) {
       --animate-index: 4;
     }
-
+    
     [class*="stagger"] > *:nth-child(5) {
       --animate-index: 5;
     }
+
   `
 
   expect.assertions(2)
-  return generateUtilities(plugin, config).then((result) => {
+  return run(plugin, config).then((result) => {
     expect(result.warnings().length).toBe(0)
     expect(result.css).toMatchCss(output)
   })
